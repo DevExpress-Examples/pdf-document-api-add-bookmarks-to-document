0b64ef59c9bf6567d605c6c304dc24e0fff0d80d
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to add bookmarks to a document
+
+
+This example shows how to create bookmarks in code and add them to a document.
+
+<br/>
+
+
---
+++ VB/AddBookmarks/Program.vb
@@ -0,0 +1,25 @@
+﻿Imports DevExpress.Pdf
+
+Namespace AddBookmarks
+    Friend Class Program
+        Shared Sub Main(ByVal args() As String)
+
+            Using processor As New PdfDocumentProcessor()
+
+                ' Load a document.
+                processor.LoadDocument("..\..\Document.pdf")
+
+                ' Create bookmarks and add them to the PDF document.
+                Dim destination1 As PdfDestination = processor.CreateDestination(1, 180, 150)
+                Dim destination2 As PdfDestination = processor.CreateDestination(1, 168, 230)
+                Dim destination3 As PdfDestination = processor.CreateDestination(1, 20, 350)
+                processor.Document.Bookmarks.Add(New PdfBookmark() With {.Title = "PDF Document Processor", .Destination = destination1})
+                processor.Document.Bookmarks.Add(New PdfBookmark() With {.Title = "Display, Print and Export PDF Documents", .Destination = destination2})
+                processor.Document.Bookmarks.Add(New PdfBookmark() With {.Title = "Learn More", .Destination = destination3})
+
+                ' Save the result document.
+                processor.SaveDocument("..\..\Result.pdf")
+            End Using
+        End Sub
+    End Class
+End Namespace
