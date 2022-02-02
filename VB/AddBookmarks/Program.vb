c1bc2c2096be7e74709b2e16453449e243867e52
DevExpressExampleBot <fakeEmail>
2022-02-02 11:11:35 +0300

    Source auto update [skip ci]

--- VB/AddBookmarks/Program.vb
+++ VB/AddBookmarks/Program.vb
@@ -1,14 +1,13 @@
-﻿Imports DevExpress.Pdf
+Imports DevExpress.Pdf
 
 Namespace AddBookmarks
+
     Friend Class Program
-        Shared Sub Main(ByVal args() As String)
 
-            Using processor As New PdfDocumentProcessor()
-
+        Shared Sub Main(ByVal args As String())
+            Using processor As PdfDocumentProcessor = New PdfDocumentProcessor()
                 ' Load a document.
                 processor.LoadDocument("..\..\Document.pdf")
-
                 ' Create bookmarks and add them to the PDF document.
                 Dim destination1 As PdfDestination = processor.CreateDestination(1, 180, 150)
                 Dim destination2 As PdfDestination = processor.CreateDestination(1, 168, 230)
@@ -16,7 +15,6 @@ Namespace AddBookmarks
                 processor.Document.Bookmarks.Add(New PdfBookmark() With {.Title = "PDF Document Processor", .Destination = destination1})
                 processor.Document.Bookmarks.Add(New PdfBookmark() With {.Title = "Display, Print and Export PDF Documents", .Destination = destination2})
                 processor.Document.Bookmarks.Add(New PdfBookmark() With {.Title = "Learn More", .Destination = destination3})
-
                 ' Save the result document.
                 processor.SaveDocument("..\..\Result.pdf")
             End Using
